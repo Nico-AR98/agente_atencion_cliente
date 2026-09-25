dee993b4f149849dea089fbe86fd2689f81ae825
Nicolás Gallardo <ngallardo@leafnoise.io>
2026-09-25 20:22:35 -0300

    modify generate_voice

--- speech/voice_generation_gtts.py
+++ speech/voice_generation_gtts.py
@@ -1,9 +1,19 @@
 import os, tempfile
 from gtts import gTTS
 
+'''
 def generate_voice(text):
     with tempfile.NamedTemporaryFile(delete=True, suffix=".mp3") as fp:
         tts = gTTS(text=text, lang="es", tld="com.ar")
         tts.save(fp.name)
 
         os.system(f"mpg123 -q {fp.name}")
+'''
+
+def generate_voice(text) -> bytes:
+    import io
+    buffer = io.BytesIO()
+    gTTS(text=text, lang="es", tld="com.ar").write_to_fp(buffer)
+    return buffer.getvalue()
+
+    
